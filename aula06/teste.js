82177836dd25a748a0baaf6ad533fc83d7f1fedd
Joice Marina <joicemarinag@gmail.com>
2023-02-24 11:21:19 -0300

    Aula 06

---
+++ aula06/teste.js
@@ -0,0 +1,19 @@
+
+// let s = 'JavaScript'
+
+// console.log(s.length)
+
+// let num = 1350.5
+
+// console.log(num.toFixed(2).replace('.', ','))
+// console.log(num.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+
+// let n1 = 5
+// let n2 = 3
+
+// let res = n1 + n2 / 2
+// console.log(5 !== '5.5')
+
+let media = 10
+
+media >= 7 ? console.log("Aprovado") : console.log("Reprovado")
