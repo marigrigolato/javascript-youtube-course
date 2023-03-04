e96fd1d0ddf5cacbca6479e95bbde86baa7f64a0
Joice Marina <joicemarinag@gmail.com>
2023-03-04 15:49:11 -0300

    Deleta arquivos pertencentes a outro curso

--- methods/includes.js
+++
@@ -1,6 +0,0 @@
-// methods includes(item) returns true if the provided array contains the specified item, false otherwise.
-
-const groceries = ["Apple", "Peach", "Tomato"];
-
-groceries.includes("Tomato"); // true
-groceries.includes("Bread"); // false
--- objects/defaultparameters.js
+++
@@ -1,9 +0,0 @@
-// default function parameters  allow named parameters to be initialized with default values
-// if no value or undefined is passed.
-
-function welcomeUser(name = "user") {
-  return `Hello ${name}`;
-}
-
-welcomeUser("Sam"); // "Hello Sam"
-welcomeUser(); // "Hello user"
